bbaddcebf837070a769a1bd08c6f33038d2cb09a
kouki <k0uki221@gmail.com>
2018-02-23 02:31:30 +0900

    Fetch zaim history as json

--- lib/zaim_history/client.rb
+++ lib/zaim_history/client.rb
@@ -1,6 +1,7 @@
 require "mechanize"
 require 'logger'
 require 'rack'
+require 'json'
 
 module ZaimHistory
   class Client
@@ -19,7 +20,27 @@ module ZaimHistory
       oauth_verifier = page2.search('code')[0].content
       url = "https://zaim.net/user_session/callback?oauth_token=#{oauth_token}&oauth_verifier=#{oauth_verifier}"
 
-      home_page = agent.get(url)
+      @home_page = agent.get(url)
+    end
+
+    def history
+      money_page = @home_page.link_with(href: "/money").click
+      result = []
+      money_page.search('table.list tr').each do |tr|
+        if date = tr.children.at('.date .edit-money')
+          r = {}
+          r[:date] = date.content
+          r[:category1] = tr.children.at('.category span').attribute('alt').value
+          r[:category2] = tr.children.at('.category .edit-money').content
+          r[:price] = tr.children.at('.price .edit-money').content
+          r[:from_acount] = tr.children.at('.from_account .account-sm')&.attribute('alt')&.value
+          r[:to_account] = tr.children.at('.to_account .account-sm')&.attribute('alt')&.value
+          r[:place] = tr.children.at('.place .edit-money span')&.attribute('title')&.value
+          r[:comment] = tr.children.at('div.comment .edit-money span')&.attribute('title')&.value
+          result << chomp_result(r)
+        end
+      end
+      result.to_json
     end
 
     private
@@ -27,6 +48,10 @@ module ZaimHistory
       @agent ||= Mechanize.new
       @agent.log = Logger.new $stderr
       @agent
+    end
+
+    def chomp_result(result)
+      result.map {|key,val| [key, val&.chomp]}.to_h
     end
   end
 end
